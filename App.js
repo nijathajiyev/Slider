5f2a917afef01da20c18c6065caa30e718d66930
Nijat <92479797+nijathajiyev@users.noreply.github.com>
2023-12-16 15:40:40 +0400

    Initial commit

---
+++ App.js
@@ -0,0 +1,62 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import CustomImageCarousal from './src/CustomImageCarousal';
+
+const  App = () => {
+
+  const data2 = [
+    {
+      image: require('./src/assets/image/image-product-1-landscape.jpg'),
+    },
+    {
+      image: require('./src/assets/image/image-product-2-landscape.jpg'),
+    },
+    {
+      image: require('./src/assets/image/image-product-3-landscape.jpg'),
+    },
+    {
+      image: require('./src/assets/image/image-product-4-landscape.jpg'),
+    },
+  ];
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.carouselContainer}>
+        <CustomImageCarousal data={data2} />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal:10
+  },
+  carouselContainer: {
+    marginBottom: 20,
+  },
+});
+
+export default App;
---
+++ src/CustomImageCarousal/index.js
@@ -0,0 +1,113 @@
+import {View, useWindowDimensions} from 'react-native';
+import React, {useState, useEffect, useRef} from 'react';
+import Animated, {
+  useSharedValue,
+  useAnimatedScrollHandler,
+  useAnimatedRef,
+} from 'react-native-reanimated';
+import Pagination from './Pagination';
+import CustomImage from './CustomImage';
+import axios from 'axios';
+
+const CustomImageCarousal = ({data}) => {
+  const scrollViewRef = useAnimatedRef(null);
+  const interval = useRef();
+   const [slide, setSlide] = useState([]);
+//   const [isAutoPlay, setIsAutoPlay] = useState(autoPlay);
+  const [newData, setNewData] = useState([
+    {key: 'spacer-left'},
+    ...slide,
+    {key: 'spacer-right'},
+  ]);
+  const {width} = useWindowDimensions();
+  const SIZE = width * 0.7;
+  const SPACER = (width - SIZE) / 2;
+  const x = useSharedValue(0);
+  const offSet = useSharedValue(0);
+
+    useEffect(() => {
+      slider();
+    }, []);
+
+    const slider = () => {
+        axios({
+          method: 'get',
+          url: 'https://picsum.photos/v2/list',
+        }).then(response => {
+          console.log(response.data, 'picsum');
+          const images = response.data.map(item=>{
+            return {
+              image: item.download_url,
+              width: item.width,
+              height: item.height,
+            };
+          });
+        //   console.log(images);
+          setSlide(images);
+        });
+    };
+
+  // Update newData if data change
+  useEffect(() => {
+    setNewData([{key: 'spacer-left'}, ...slide, {key: 'spacer-right'}]);
+  }, [slide]);
+
+  const onScroll = useAnimatedScrollHandler({
+    onScroll: event => {
+      x.value = event.contentOffset.x;
+    },
+    onMomentumEnd: e => {
+      offSet.value = e.contentOffset.x;
+    },
+  });
+
+//   useEffect(() => {
+//     if (isAutoPlay === true) {
+//       let _offSet = offSet.value;
+//       interval.current = setInterval(() => {
+//         if (_offSet >= Math.floor(SIZE * (data.length - 1) - 10)) {
+//           _offSet = 0;
+//         } else {
+//           _offSet = Math.floor(_offSet + SIZE);
+//         }
+//         scrollViewRef.current.scrollTo({x: _offSet, y: 0});
+//       }, 2000);
+//     } else {
+//       clearInterval(interval.current);
+//     }
+//     return () => {
+//       clearInterval(interval.current);
+//     };
+//   }, [SIZE, SPACER, isAutoPlay, data.length, offSet.value, scrollViewRef]);
+
+  return (
+    <View>
+      <Animated.ScrollView
+        ref={scrollViewRef}
+        onScroll={onScroll}
+        scrollEventThrottle={16}
+        decelerationRate="fast"
+        snapToInterval={SIZE}
+        horizontal
+        bounces={false}
+        showsHorizontalScrollIndicator={false}>
+        {newData.map((item, index) => {
+          return (
+            <CustomImage
+              slide={slide}
+              key={index}
+              index={index}
+              item={item}
+              x={x}
+              size={SIZE}
+              spacer={SPACER}
+            />
+          );
+        })}
+      </Animated.ScrollView>
+      {/* <Pagination data={slide} x={x} size={SIZE} /> */}
+    </View>
+  );
+};
+
+export default CustomImageCarousal;
